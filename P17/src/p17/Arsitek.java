6092a0eadb165f131cc6381667d95ff6963dfbe7
Hamdandi <90332110+Hamdandi@users.noreply.github.com>
2021-11-26 14:58:16 +0700

    Learning about Factory pattern

---
+++ P17/src/p17/Arsitek.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p17;
+
+/**
+ *
+ * @author mhdha
+ */
+public class Arsitek implements Laptop {
+
+    @Override
+    public void runTest() {
+        System.out.println("Running test on ARSITEK laptop");
+    }
+    
+}
---
+++ P17/src/p17/LaptopFactory.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p17;
+
+/**
+ *
+ * @author mhdha
+ */
+public class LaptopFactory {
+    public Laptop createLaptop(LaptopType laptopType){
+        Laptop laptop = null;
+        switch (laptopType){
+            case GAMING:
+                laptop = new Gaming();
+                break;
+            case MULTIMEDIA:
+                laptop = new Multimedia();
+                break;
+            case ARSITEK:
+                laptop = new Arsitek();
+                break;
+            default:
+                break;
+        }
+        return laptop;
+    }
+}
